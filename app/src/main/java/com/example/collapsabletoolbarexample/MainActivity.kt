fb566934f1c96370e20583e3a192c7e75887b10f
Anmol Singh Sahi <sahi.anmool@gmail.com>
2021-06-19 23:52:18 +0530

    initial commit

---
+++ app/src/main/java/com/example/collapsabletoolbarexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.collapsabletoolbarexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
